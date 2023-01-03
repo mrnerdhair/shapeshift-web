cf51336922779f8ea02d78d7c93313e125372ceb
reallybeard <89934888+reallybeard@users.noreply.github.com>
2023-01-03 16:26:11 -0600

    filter out tvl and clear the opportunities

--- src/state/migrations/index.ts
+++ src/state/migrations/index.ts
@@ -1,5 +1,7 @@
 import { clearOpportunitiesSlice } from './000_clear_opportunities_slice'
+import { clearDeprecatedYearnOpportunities } from './000_clear_yearn_opportunities_slice'
 
 export const migrations = {
   0: clearOpportunitiesSlice,
+  1: clearDeprecatedYearnOpportunities,
 }
